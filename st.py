c09655fc672da83425c4ee357b1026ddaa8cc84a
shaivy <shaivygowda@gmail.com>
2023-08-02 17:48:44 +0530

    stashed 1

--- st.py
+++ st.py
@@ -1,2 +1,8 @@
 print("hello world")
+<<<<<<< Updated upstream
 print("python program is easy")
+=======
+print("python program")
+print("this is master branch")
+print("fortrum program")
+>>>>>>> Stashed changes
