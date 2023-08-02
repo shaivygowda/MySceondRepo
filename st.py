698610095cdf738b658d366d564c23203cb2179a
shaivy <shaivygowda@gmail.com>
2023-08-02 17:55:54 +0530

    stashed 4

--- st.py
+++ st.py
@@ -2,5 +2,10 @@ print("hello world")
 print("python program")
 print("this is master branch")
 print("fortrum program")
+<<<<<<< Updated upstream
 print("abc")
 print("def")
+=======
+>>>>>>> Stashed changes
+print("xyz")
+>>>>>>> Stashed changes
