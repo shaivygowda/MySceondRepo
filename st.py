d097d869228035bc7830d574f4d291f86600a178
shaivy <shaivygowda@gmail.com>
2023-08-04 17:45:51 +0530

    git repo

--- st.py
+++ st.py
@@ -1,8 +1,7 @@
 print("hello world")
-<<<<<<< Updated upstream
-print("python program is easy")
-=======
 print("python program")
 print("this is master branch")
 print("fortrum program")
->>>>>>> Stashed changes
+
+
+print(" git hub repo")
